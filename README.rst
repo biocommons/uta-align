6fb8de32490710856637f3bcb140fa7ad5872f67
Reece Hart <reecehart@gmail.com>
2020-03-03 10:57:38 -0800

    Implemented cython setup with cython_setuptools

--- README.rst
+++ README.rst
@@ -24,9 +24,27 @@ Install developer environment::
 
   $ make devready
 
+Activate the environment::
+
+  $ source venv/3.7/bin/activate
+
+(or other python version as appropriate for your system)
+
 Test it::
 
-  $ pytest
+  $ make test
+  ======================================== test session starts ========================================
+  platform linux -- Python 3.7.5, pytest-5.3.5, py-1.8.1, pluggy-0.13.1
+  rootdir: /home/reece/projects/biocommons/uta-align, inifile: setup.cfg, testpaths: tests
+  collected 5 items                                                                                   
+   
+  tests/test_align_algorithms.py .                                                              [ 20%]
+  tests/test_align_gap_open.py ..                                                               [ 60%]
+  tests/test_cigar_utils.py .                                                                   [ 80%]
+  tests/test_nw.py .                                                                            [100%]
+   
+  ========================================= 5 passed in 0.12s =========================================
+
 
 Because cython (.pyx) files are compiled, you'll need to reinstall the
 development package after code changes, like this::
--- setup.py
+++ setup.py
@@ -1,61 +1,6 @@
-import glob
-import importlib
-import logging
-import subprocess
-
-from setuptools import find_packages, setup, Extension
-
-
-# N.B. log messages typically hidden; use `pip install -v` to see
-_logger = logging.getLogger()
-
-
-try:
-    import pysam
-except ModuleNotFoundError:
-    _logger.critical("!! pysam must be installed prior to running setup.py (pip install pysam)")
-    raise
-
-try:
-    from Cython.Build import cythonize, build_ext
-    has_cython = True
-    _logger.info("# cython available; will cythonize .pyx sources")
-except ImportError:
-    has_cython = False
-    _logger.info("# cython unavailable; using included .c sources")
-
-
-
-# ###########################################################################
-# extnsion setup
-# https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#configuring-the-c-build
-# http://docs.cython.org/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
-
-glob_ext = "pyx" if has_cython else "c"
-src_glob = "uta_align/align/*." + glob_ext
-
-if has_cython:
-    sources = [src_glob]
-else:
-    sources = glob.glob(src_glob)
-
-extensions = [
-    Extension("uta_align", sources,
-              include_dirs = pysam.get_include())
-#              libraries = [pysam.libchtslib.__file__])
-    ]
-
-if has_cython:
-    compiler_directives = {
-        'c_string_encoding': 'ascii',
-        'embedsignature': True,
-        'language_level': 3
-        }
-    extensions = cythonize(extensions, compiler_directives=compiler_directives)
-
+from cython_setuptools.vendor import setup
 
 setup(
-    ext_modules=extensions,
     use_scm_version=True,
 )
 
